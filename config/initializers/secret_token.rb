b1c65661c42723a64f223594539ddea473a66d54
Vince The Mac Vincent <mellterm@gmail.com>
2012-12-24 13:34:03 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Termz::Application.config.secret_token = 'e25e7c5e1fac38632cc48dadf91fd6edf01d7a4c4cd3f66b4859b4a093cb5b8e7f8a10f3a55ec841b88d654067492567627b717d2cca2d92a5881ce94e104325'
