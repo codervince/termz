7ff050f07660b64b8300a7323db0fe9020e11778
Vince The Mac Vincent <mellterm@gmail.com>
2013-01-07 22:42:25 +0700

    add rspec and cucumber scenarios for user login/signup/signout/view/edit pages

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,11 +2,23 @@ require 'spec_helper'
 
 describe UsersController do
 
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
+  before (:each) do
+    @user = FactoryGirl.create(:user)
+    sign_in @user
+  end
+
+  describe "GET 'show'" do
+    
+    it "should be successful" do
+      get :show, :id => @user.id
       response.should be_success
     end
+    
+    it "should find the right user" do
+      get :show, :id => @user.id
+      assigns(:user).should == @user
+    end
+    
   end
 
 end
